f28c1ea713e4ab745e73c7655830a060bd6c6863
dylan-brackett <dylan.t.brackett@gmail.com>
2022-09-23 16:20:49 +0000

    Add excercise

--- src/assembly_line.rs
+++ src/assembly_line.rs
@@ -3,9 +3,17 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let prod_rate: f64 = 221.0;
+    let sucess_rate: f64 = match speed {
+        0..=4  => 1.0,
+        5..=8  => 0.9,
+        9..=10 => 0.77,
+            _  => 0.0,
+    };
+
+    speed as f64 * prod_rate * sucess_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60.0) as u32
 }
